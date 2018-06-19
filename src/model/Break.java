ee079089de4c66ccd3b72e0260d49f6f9be2cdd1
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-06-19 08:21:25 -0700

    استفاده از گتر و ستر

--- src/model/Break.java
+++ src/model/Break.java
@@ -5,4 +5,18 @@ public class Break {
      * model
      * power    (between 0 and 10)
      */
+    public int model;
+
+    private int power;
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        if (power > 0 && power < 10)
+            return;
+
+        this.power = power;
+    }
 }
