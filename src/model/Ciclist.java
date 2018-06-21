a18c03f8372785d750996e25eb578e1465cc4229
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-06-21 08:31:15 -0700

    کلاس دوچرخه سوار تکمیل شد

--- src/model/Ciclist.java
+++ src/model/Ciclist.java
@@ -3,12 +3,62 @@ package model;
 public class Ciclist {
     /**
      * name
-     *
-     *
+     * roundPerMinutes
      * pushPower
      * bike
+     * speed
      *
      * pedal()
      * breaking()
      */
+    private String name;
+    private int pushPower;
+    private int roundPerMinutes;
+
+    public int getRoundPerMinutes() {
+        return roundPerMinutes;
+    }
+
+    public void setRoundPerMinutes(int roundPerMinutes) {
+        this.roundPerMinutes = roundPerMinutes;
+    }
+
+    private Bike bike;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPushPower() {
+        return pushPower;
+    }
+
+    public void setPushPower(int pushPower) {
+        this.pushPower = pushPower;
+    }
+
+    public Bike getBike() {
+        return bike;
+    }
+
+    public void setBike(Bike bike) {
+        this.bike = bike;
+    }
+
+    public int getSpeed(){
+        return bike.speed;
+    }
+
+    public void pedal(){
+        bike.pedal(roundPerMinutes);
+    }
+
+    public void breaking(){
+        bike.breaking(pushPower);
+    }
+
 }
